d78860c89ca274047c300a6df8a03e067bd48479
kalmanb <kalman@movio.co>
2016-06-27 17:08:59 +1200

    Add Model description and parameters

--- src/application/components/Application/Model/index.js
+++ src/application/components/Application/Model/index.js
@@ -3,14 +3,20 @@ import React, { PropTypes } from 'react';
 import H1 from '../../../../components/H1';
 import H2 from '../../../../components/H2';
 import JsonDoc from '../../JsonDoc';
+import ParameterList from '../../ParameterList';
+
+import styles from './model.css';
 
 const Model = ({ modelName, spec }) => {
   const model = spec.models.find(m => m.name === modelName);
   return (
     <div>
       <H1>{model.name}</H1>
-      
+      {model.description ? <p className={styles.description}>{model.description}</p> : null}
       <H2>Fields</H2>
+      {model.fields.map((field, id) => (
+          <ParameterList {...field} />
+      ))}
       <JsonDoc baseModel={model.name} spec={spec} />
     </div>
   );
