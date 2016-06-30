64ad9bb9dc1fff1d4389210cefb5b7466b66a594
jackhopner <jack@movio.co>
2016-06-30 16:06:31 +1200

    Fix syntax error

--- src/application/components/ParameterList/index.js
+++ src/application/components/ParameterList/index.js
@@ -11,7 +11,7 @@ const ParameterList = ({ name, type, required, description, example, defaultValu
       {required ? <p className={styles.required}>required</p> : null}
     </div>
     <div className={styles.info}>
-      description ? <ReactMarkdown source={description} className={styles.description} /> : null}
+      {description ? <ReactMarkdown source={description} className={styles.description} /> : null}
       {example ? <p className={styles.example}>Example: {example}</p> : null}
       {defaultValue ? <p className={styles.default}>Default: {defaultValue}</p> : null}
     </div>
