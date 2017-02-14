bb874dc1ae69af374b415206e30f1e1596307ac4
kalmanb <kalman@movio.co>
2017-02-14 14:56:49 +1300

    Update libs and flowtypes

--- src/components/LoadingOverlay/index.js
+++ src/components/LoadingOverlay/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 
 import styles from './loading-overlay.css';
 
 const LoadingOverlay = () =>
-  <div className={styles.container}>
-    <div className={styles.loader}>Loading</div>
-  </div>;
+  <ReactCSSTransitionGroup
+          transitionName='_loading-overlay-transition'
+          transitionAppear
+          transitionEnterTimeout={500}
+          transitionLeaveTimeout={500}
+          transitionAppearTimeout={500}
+          component={FirstChild}
+          >
+    <div className={styles.container}>
+      <div className={styles.loader}>Loading</div>
+    </div>
+  </ReactCSSTransitionGroup>;
 
 export default LoadingOverlay;
