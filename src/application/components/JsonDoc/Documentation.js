c37fabecd8b4663cc587a0c06c5aaf370c69339f
Jacky Zhen <jacky.s.zhen@gmail.com>
2017-04-23 10:14:49 +1200

    document container to remember greatest height
    Fixes a documentation component hover UI bug when a json doc is rendered at the bottom
    of the page. The bug is caused by the height of the component being
    dynamic based on content, which extends or contracts the page overall
    height.

--- src/application/components/JsonDoc/Documentation.js
+++ src/application/components/JsonDoc/Documentation.js
@@ -1,5 +1,6 @@
 // @flow
-import React from 'react';
+import React, { Component } from 'react';
+import classnames from 'classnames';
 
 import ParameterList from '../ParameterList';
 import * as utils from '../../../utils';
@@ -12,23 +13,57 @@ const defaultView =
     <span className={styles.defaultView}>&lt;-- Hover over JSON example for documentation</span>
   </div>);
 
-const Documentation = ({ documentationFullType, service, importedServices }: {
+type Props = {
   documentationFullType: string,
   service: Service,
   importedServices: Service[],
-}) => {
-  if (!documentationFullType) return defaultView;
-
-  const modelName = documentationFullType.substring(0, documentationFullType.lastIndexOf('.'));
-  const fieldName = documentationFullType.substring(documentationFullType.lastIndexOf('.') + 1);
-  const model = utils.getModel(modelName, service, importedServices);
-  const field = model ? model.fields.find(f => f.name === fieldName) : null;
-
-  return (
-    <div className={styles.documentation}>
-      <ParameterList {...field} service={service} importedServices={importedServices} parentModel={modelName} />
-    </div>
-  );
 };
+
+class Documentation extends Component {
+  props: Props;
+  state: {
+    height: number,
+  };
+  constructor(props: props) {
+    super(props);
+    this.state = { height: 0 };
+    this.updateContainerHeight = this.updateContainerHeight.bind(this);
+  }
+
+  componentDidUpdate() {
+    this.updateContainerHeight();
+  }
+
+  updateContainerHeight() {
+    const container = this.container;
+    if (container) {
+      const height = container.clientHeight;
+      if (height > this.state.height) {
+        this.setState({ height });
+      }
+    }
+  }
+
+  render() {
+    const { documentationFullType, service, importedServices } = this.props;
+    const { height } = this.state;
+    if (!documentationFullType) return defaultView;
+
+    const modelName = documentationFullType.substring(0, documentationFullType.lastIndexOf('.'));
+    const fieldName = documentationFullType.substring(documentationFullType.lastIndexOf('.') + 1);
+    const model = utils.getModel(modelName, service, importedServices);
+    const field = model ? model.fields.find(f => f.name === fieldName) : null;
+
+    return (
+      <div
+        ref={(el) => { this.container = el; }}
+        className={classnames(styles.documentation)}
+        style={{ minHeight: `${height}px` }}
+      >
+        <ParameterList {...field} service={service} importedServices={importedServices} parentModel={modelName} />
+      </div>
+    );
+  }
+}
 
 export default Documentation;
