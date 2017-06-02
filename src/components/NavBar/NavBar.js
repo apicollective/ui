65e2eea4be3782eb544222a8296763dd9e15f60e
kalmanb <kalman@movio.co>
2017-06-03 09:21:19 +1200

    NavBar to react-router-v4

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,92 +1,29 @@
 // @flow
 import React from 'react';
-import { connect } from 'react-redux';
+import { Route, Switch } from 'react-router-dom';
 import NavBarContents from 'components/NavBar/NavBarContents';
-import * as utils from 'utils';
 
-import type { Props } from 'params';
-import type { NavItem } from 'nav/NavItem';
-import type { State } from 'app/reducers';
-
-const NavBar = (props: Props) => {
-  const title = process.env.TITLE ? process.env.TITLE : 'apidoc';
-  let removeGithubLink = true;
-  if (typeof process.env.TITLE === 'undefined' || !process.env.TITLE) {
-    removeGithubLink = false;
-  }
-  return (
-    <NavBarContents
-      title={title}
-      items={getItems(props)}
-      toHref={'/'}
-      removeGithubLink={removeGithubLink}
+const NavBar = () => (
+  <Switch>
+    <Route
+      path="/org/:organizationKey/app/:applicationKey/r/:resource/m/:method/p/:path"
+      component={NavBarContents}
     />
-  );
-};
+    <Route
+      path="/org/:organizationKey/app/:applicationKey/m/:model"
+      component={NavBarContents}
+    />
+    <Route
+      path="/org/:organizationKey/app/:applicationKey"
+      component={NavBarContents}
+    />
+    {/* <Route
+          path="/org/:organizationKey/doc/:documentationKey"
+          component={Documentation}
+          /> */}
+    <Route path="/org/:organizationKey" component={NavBarContents} />
+    <Route component={NavBarContents} />
+  </Switch>
+);
 
-const getItems = (props: Props): NavItem[] => {
-  // FIXME test
-  if (!props.match || !props.service || !props.service.apidoc) {
-    return [];
-  }
-  // FIXME test
-  let operationPath = '';
-  const params = props.match.params;
-  if (params.resource) {
-    const op = utils.getOperation(
-      params.resource,
-      params.method,
-      params.path,
-      props.service
-    );
-    operationPath = op.path;
-  }
-
-  return [].concat(
-    params.organizationKey
-      ? {
-          name: params.organizationKey,
-          toHref: utils.buildNavHref({
-            organization: params.organizationKey,
-          }),
-        }
-      : [],
-    params.applicationKey
-      ? {
-          name: params.applicationKey,
-          toHref: utils.buildNavHref({
-            organization: params.organizationKey,
-            application: params.applicationKey,
-          }),
-        }
-      : [],
-    params.resource
-      ? {
-          name: `${params.method.toUpperCase()} ${operationPath}`,
-          toHref: utils.buildNavHref({
-            organization: params.organizationKey,
-            application: params.applicationKey,
-            resource: params.resource,
-            method: params.method,
-            path: params.path,
-          }),
-        }
-      : [],
-    params.model
-      ? {
-          name: `${params.model}`,
-          toHref: utils.buildNavHref({
-            organization: params.organizationKey,
-            application: params.applicationKey,
-            model: params.model,
-          }),
-        }
-      : []
-  );
-};
-
-const mapStateToProps = (state: State) => ({
-  service: state.application.service,
-});
-
-export default connect(mapStateToProps)(NavBar);
+export default NavBar;
